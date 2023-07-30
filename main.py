b6c98f67962c556e6ed4cbdfc746151d921a9978
kiran narayan <kiran.narayan71@gmail.com>
2023-07-30 14:13:42 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import sys
 from ConcreteStrength.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ConcreteStrength.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from ConcreteStrength import logger, CustomException
 
-STAGE_NAME = 'DATA_INGESTION'
+
 
 if __name__ == '__main__':
     try:
+        STAGE_NAME = 'DATA_INGESTION'        
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataIngestionTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+        STAGE_NAME = 'DATA_VALIDATION'
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
     except Exception as e:
         logger.error(CustomException(e, sys))
         raise e
